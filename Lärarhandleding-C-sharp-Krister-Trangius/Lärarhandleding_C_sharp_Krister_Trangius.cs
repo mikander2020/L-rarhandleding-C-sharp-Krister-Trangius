bb116fe836586b07f500546478ae85af48d4e493
mian0003 <mian0003@5CG5021CLJ>
2017-07-29 11:54:05 +0200

    kap6 och kap 7

--- Lärarhandleding-C-sharp-Krister-Trangius/Lärarhandleding_C_sharp_Krister_Trangius.cs
+++ Lärarhandleding-C-sharp-Krister-Trangius/Lärarhandleding_C_sharp_Krister_Trangius.cs
@@ -1,6 +1,27 @@
 ﻿namespace Lärarhandleding_C_sharp_Krister_Trangius {
     class Lärarhandleding_C_sharp_Krister_Trangius {
         static void Main(string[] args) {
+            // kap. 6
+            // System.Console.WriteLine("Hello World");
+
+            // kap. 7
+            //System.Console.Write("Ge ett tal: ");
+            //double tal1 = System.Convert.ToDouble(System.Console.ReadLine());
+            //System.Console.Write("Ge ett tal till: ");
+            //double tal2 = System.Convert.ToDouble(System.Console.ReadLine());
+
+            //System.Console.WriteLine("Summan är " + (tal1 + tal2));
+
+            System.Console.Write("Ge ett tal: ");
+            decimal tal1 = System.Convert.ToDecimal(System.Console.ReadLine());
+            System.Console.Write("Ge ett tal till: ");
+            decimal tal2 = System.Convert.ToDecimal(System.Console.ReadLine());
+            System.Console.Write("Ge ett tredje tal: ");
+            decimal tal3 = System.Convert.ToDecimal(System.Console.ReadLine());
+
+            decimal medelvärde = (tal1 + tal2+ tal3)/3;
+            System.Console.WriteLine("Medelvärdet är " + System.Math.Round(medelvärde, 2));
+
         }
     }
 }
